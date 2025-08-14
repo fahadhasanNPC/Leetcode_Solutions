50748668526043d9664aee9f0a6dda2d298bf9b1
Md. Fahad Hasan <108455971+fahadhasanNPC@users.noreply.github.com>
2025-08-14 21:34:15 +0600

    Medium
    Runtime - 0 ms

---
+++ 74-Search a 2D Matrix.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int i = m - 1, j = 0;
+
+        while (i >= 0 && j < n) {
+            if (matrix[i][j] == target) {
+                return true;
+            } else if (matrix[i][j] < target) {
+                j++;
+            } else {
+                i--;
+            }
+        }
+        return false;
+    }
+};
+
