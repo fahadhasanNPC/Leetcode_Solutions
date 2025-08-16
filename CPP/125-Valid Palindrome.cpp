ac059db98ef1348342da02bd0bb19bdb24fa96f0
Md. Fahad Hasan <108455971+fahadhasanNPC@users.noreply.github.com>
2025-08-16 22:20:30 +0600

    Easy

---
+++ CPP/125-Valid Palindrome.cpp
@@ -0,0 +1,26 @@
+#include <cctype>  
+#include <string>
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string filtered = "";
+        
+        
+        for (char c : s) {
+            if (isalnum(c)) {  
+                filtered += tolower(c);  
+            }
+        }
+
+        
+        int left = 0, right = filtered.length() - 1;
+        while (left < right) {
+            if (filtered[left++] != filtered[right--]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
