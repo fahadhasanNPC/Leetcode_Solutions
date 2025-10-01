85a91f654c4e9fc8df2c80b5b11d795abaca9680
Md. Fahad Hasan <108455971+fahadhasanNPC@users.noreply.github.com>
2025-10-02 00:11:40 +0600

    Medium

---
+++ CPP/204-CountPrime.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPrime(int num) {
+    if (num < 2) return false;
+    for (int i = 2; i*i <= num; ++i) {
+        if (num % i == 0) return false;
+    }
+    return true;
+}
+
+int countPrimes(int n) {
+    int count = 0;
+    for (int i = 2; i < n; ++i) {
+        if (isPrime(i)) count++;
+    }
+    return count;
+}
+        
+    
+};
